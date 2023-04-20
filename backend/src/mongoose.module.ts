399b6c9510a373bfe8a0425cc9e8a5b9c2f0ab41
isaias santos <84031593+isaias-silva@users.noreply.github.com>
2023-04-20 12:32:27 +0000

    insunidade

--- backend/src/mongoose.module.ts
+++ backend/src/mongoose.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost/kuma_data')],
+    imports: [MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@data.djsnl.mongodb.net/?retryWrites=true&w=majority`)],
   })
   export class MongooseConfigModule {}
